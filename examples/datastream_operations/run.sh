491a895278a44315490d7e62859027e274599220
nasirali1 <nasir.ali08@gmail.com>
2019-02-14 14:56:58 -0600

    changed hard coded path to placeholders

--- examples/datastream_operations/run.sh
+++ examples/datastream_operations/run.sh
@@ -8,10 +8,10 @@
 export PYSPARK_PYTHON=/usr/bin/python3
 
 # export CerebralCortex path
-export PYTHONPATH="${PYTHONPATH}:/IdeaProjects/CerebralCortex-Kernel-Examples/"
+export PYTHONPATH="${PYTHONPATH}:/MAIN-DIR-PATH/CerebralCortex-Kernel-Examples/"
 
 #Spark path, uncomment if spark home is not exported else where.
-export SPARK_HOME=/home/ali/spark/spark-2.4.0-bin-hadoop2.7
+export SPARK_HOME=/MAIN-DIR-PATH/spark-2.4.0-bin-hadoop2.7
 
 # spark master. This will work on local machine only. In case of cloud, provide spark master node URL:port.
 SPARK_MASTER="local[*]"
