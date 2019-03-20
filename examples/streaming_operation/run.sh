7d0e74c6ef344e36a55b88718460ebe0ebf9b1e5
nasirali1 <nasir.ali08@gmail.com>
2019-03-20 11:49:38 -0500

    added development branch

--- examples/streaming_operation/run.sh
+++ examples/streaming_operation/run.sh
@@ -3,8 +3,14 @@
 # Python3 path
 export PYSPARK_PYTHON=/usr/bin/python3
 
+# export CerebralCortex path if CerebralCortex is not installed
+export PYTHONPATH="${PYTHONPATH}:/home/ali/IdeaProjects/CerebralCortex-2.0"
+#
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/hadoop/lib/native/libhdfs.so
+##sudo ln -s /usr/local/hadoop/lib/native/libhdfs.so /usr/lib/libhdfs.so
+
 #Spark path
-export SPARK_HOME=/MAIN-DIR-PATH/spark-2.4.0-bin-hadoop2.7/
+export SPARK_HOME=/home/ali/spark/spark-2.4.0-bin-hadoop2.7/
 
 #PySpark args (do not change unless you know what you are doing)
 export PYSPARK_SUBMIT_ARGS="--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0 pyspark-shell"
